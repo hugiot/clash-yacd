2a3fa64b1d563d8ad55705bd05ce1df8475a8641
xiaocai <1643013940@qq.com>
2023-09-12 14:21:08 +0800

    edit install.sh

--- install.sh
+++ install.sh
@@ -7,38 +7,34 @@ packageManager=""
 command -v apt > /dev/null 2>&1 && packageManager=apt || packageManager=yum
 
 # 工具安装（wget、unzip）
-echo "============================================"
-echo "install wget、unzip"
-echo "============================================"
-sleep 1s
+echo "+----------------------------------------------------"
+echo "| install wget、unzip"
+echo "+----------------------------------------------------"
+sleep 2s
 ${packageManager} -y install wget unzip
 
 # 下载压缩包
-echo "============================================"
-echo "download clash-yacd.zip"
-echo "============================================"
-sleep 1s
+echo "+----------------------------------------------------"
+echo "| download clash-yacd.zip"
+echo "+----------------------------------------------------"
+sleep 2s
 wget -O clash-yacd.zip https://github.com/hugiot/clash-yacd/releases/download/v1.18.0/clash-yacd.zip
 
 # 解压
-echo "============================================"
-echo "unzip clash-yacd.zip"
-echo "============================================"
-sleep 1s
+echo "+----------------------------------------------------"
+echo "| unzip clash-yacd.zip"
+echo "+----------------------------------------------------"
+sleep 2s
 unzip -o clash-yacd.zip -d /opt/clash
 
 # 移除下载包
-echo "============================================"
-echo "remove clash-yacd.zip"
-echo "============================================"
-sleep 1s
+echo "+----------------------------------------------------"
+echo "| remove clash-yacd.zip"
+echo "+----------------------------------------------------"
+sleep 2s
 rm -rf clash-yacd.zip
 
 # 添加可执行权限
-echo "============================================"
-echo "add execute permission"
-echo "============================================"
-sleep 1s
 chmod +x /opt/clash/bin/*
 
 # 获取系统架构
@@ -50,16 +46,18 @@ case $(uname -m) in
     arch=arm64
     ;;
   *)
-    echo "Error: unknown system architecture"
+    echo "+----------------------------------------------------"
+    echo "| Error: unknown system architecture"
+    echo "+----------------------------------------------------"
     exit 1
     ;;
 esac
 
 # 添加服务
-echo "============================================"
-echo "add service"
-echo "============================================"
-sleep 1s
+echo "+----------------------------------------------------"
+echo "| add clash service"
+echo "+----------------------------------------------------"
+sleep 2s
 touch /usr/lib/systemd/system/clash.service
 cat > /usr/lib/systemd/system/clash.service <<EOF
 [Unit]
@@ -79,14 +77,14 @@ WantedBy=multi-user.target
 EOF
 
 # 重载服务
-echo "============================================"
-echo "restart service"
-echo "============================================"
-sleep 1s
+echo "+----------------------------------------------------"
+echo "| restart clash service"
+echo "+----------------------------------------------------"
+sleep 2s
 systemctl daemon-reload
 systemctl restart clash
 
-echo "============================================"
-echo "install success"
-echo "please visit: http://127.0.0.1:9090/ui"
-echo "============================================"
+echo "+----------------------------------------------------"
+echo "| install success"
+echo "| please visit: http://127.0.0.1:9090/ui"
+echo "+----------------------------------------------------"
